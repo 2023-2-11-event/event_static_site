9e84ddfd40076f571fa6fe02158291bcf18202ef
beengineer <beengineer.student@gmail.com>
2023-02-13 23:26:08 +0900

    renew

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
 window.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("test").style.fontSize = "4em"
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
-    window.alert("これはアラートです。");
+  let size =document.getElementById("test").style["fontSize"];
+  console.log(size)
+  if(size == "4em"){
+    document.getElementById("test").style.fontSize = "8em"
+  }
+
+  else if(size == "8em"){
+    document.getElementById("test").style.fontSize = "4em"
+  }
+
+    
   });
 });
