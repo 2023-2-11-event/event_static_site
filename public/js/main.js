e6c1619befa6f2a728694666f04242471276d90f
beengineer-demia <beengineer.student@gmail.com>
2023-02-13 23:37:14 +0900

    renew

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 window.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("doraemonn").style.fontSize = "5em"
   const alertBtn = document.getElementById("alert-btn");
-  alertBtn.addEventListener("click", function (e) {
-    window.alert("これはアラートです。");
+  alertBtn.addEventListener("mousemove", function (e) {
+    let size = document.getElementById("doraemonn").style["fontSize"];
+    if(size == "5em"){
+      document.getElementById("doraemonn").style.fontSize = "8em"
+    }
+    else if(size == "8em"){
+      document.getElementById("doraemonn").style.fontSize = "5em"
+    }
   });
 });
